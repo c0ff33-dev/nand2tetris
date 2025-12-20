3f9585b6941bd7505072d41feb5fcccabb442a6e
c0ff33-dev <5835162+c0ff33-dev@users.noreply.github.com>
2025-12-20 22:18:25 +1100

    Screen: drawHorizontal/Symetric()

--- interpreter/compiler.py
+++ interpreter/compiler.py
@@ -73,7 +73,10 @@ sys_func = {
                "drawRectangle": {"kind": "func", "type": "void", "args": ("int",)*4, "len": 4},
                "drawCircle": {"kind": "func", "type": "void", "args": ("int",)*3, "len": 3},
                "updateLocation": {"kind": "func", "type": "void", "args": ("int", "Array"), "len": 2}, # internal api
-               "drawConditional": {"kind": "func", "type": "void", "args": ("int", "int", "boolean"), "len": 3}}, # internal api
+               "drawConditional": {"kind": "func", "type": "void", "args": ("int", "int", "boolean"), "len": 3}, # internal api
+               "drawHorizontal": {"kind": "func", "type": "void", "args": ("int",)*3, "len": 3}, # internal api
+               "drawSymetric": {"kind": "func", "type": "void", "args": ("int",)*4, "len": 0}, # internal api
+               },
     "Keyboard": {"init": {"kind": "func", "type": "void", "args": (), "len": 0},
                  "keyPressed": {"kind": "func", "type": "char", "args": (), "len": 0},
                  "readChar": {"kind": "func", "type": "char", "args": (), "len": 0},
